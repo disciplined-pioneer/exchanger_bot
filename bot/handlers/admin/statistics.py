5707379bdf8d14de3d0f7344451ed2a340b201e7
Virtu <maksimsarsov777@gmail.com>
2025-04-24 20:08:40 +0300

    Обработка Назад

--- bot/handlers/admin/statistics.py
+++ bot/handlers/admin/statistics.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
-from bot.keyboards.user.start import back_admin_keyb
+from bot.keyboards.user.start import back_admin_keyb, start_admin_keyb
+from bot.templates.user.start import starting_admin_message
 from bot.templates.admin.statistics import get_monthly_exchange_report
 
 
@@ -9,9 +10,19 @@ router = Router()
 
 # Обработка получения статистики
 @router.callback_query(F.data == "admin_stats")
-async def ask_buttons(callback: types.CallbackQuery, state: FSMContext):
+async def ask_buttons(callback: types.CallbackQuery):
     await callback.message.edit_text(
         await get_monthly_exchange_report(),
         reply_markup=back_admin_keyb
+    )
+    await callback.answer()
+
+
+# Обработка кнопки "Назад"
+@router.callback_query(F.data == "start_menu_admin")
+async def back_buttons(callback: types.CallbackQuery):
+    await callback.message.edit_text(
+        starting_admin_message,
+        reply_markup=start_admin_keyb
     )
     await callback.answer()
