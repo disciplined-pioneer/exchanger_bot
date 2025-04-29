bb6524cae9f326bcb99500644c559ad7332b1be2
Virtu <maksimsarsov777@gmail.com>
2025-04-29 17:43:49 +0300

    UPDATE

--- bot/keyboards/partner/result_exchange.py
+++ bot/keyboards/partner/result_exchange.py
@@ -1,3 +1,4 @@
+from settings import settings
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_partial_exchange_completion_keyboard() -> InlineKeyboardMarkup:
@@ -12,6 +13,6 @@ def get_full_exchange_completion_keyboard() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text="✅ Я получил CNY, завершить обмен", callback_data="confirm_cny_received")],
-            [InlineKeyboardButton(text="❌ Я не получил, написать в поддержку", callback_data="support_not_received")]
+            [InlineKeyboardButton(text="❌ Я не получил, написать в поддержку", url=settings.bot.SUPPORT_LINK)]
         ]
     )
